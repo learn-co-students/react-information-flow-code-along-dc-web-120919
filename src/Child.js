976dad4fa05024d0d8b36ec4c135d36f83525655
John Louis Rumingan <jmrumingan@gmail.com>
2020-02-18 17:11:56 -0500

    Done.

--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
-import { getRandomColor } from './randomColorGenerator.js'
+import { getRandomColor } from './randomColorGenerator';
+// import { getRandomColor } from './randomColorGenerator.js' No longer needed, it is being passed as a prop
 
 class Child extends Component {
+
+
+  constructor(startCell){
+    super();
+    this.state = {
+      startColor: startCell.value,
+      colorChange: startCell.handleColorChange,
+    }
+  }
+
   render() {
+    console.log(`Rendering..`, this.props)
+
     return (
       <div
+        onClick={this.state.colorChange}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.state.startColor}}
       ></div>
     )
   }
--- src/Parent.js
+++ src/Parent.js
@@ -7,15 +7,23 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childColor: '#FFF'
     }
+  }
+
+  changeColor = () => {
+
+    this.setState({
+      color: getRandomColor(),
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child value={getRandomColor()} handleColorChange={this.changeColor}/>
+        <Child value={getRandomColor()} handleColorChange={this.changeColor}/>
       </div>
     )
   }
